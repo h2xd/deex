d70db07a3a7044fe84e6ed514407f4bb2354b216
Andrej Schujkow <a.schujkow@gmail.com>
2024-01-12 16:05:23 +0100

    chore: add biome

--- packages/deex/src/App.tsx
+++ packages/deex/src/App.tsx
@@ -1,30 +1,35 @@
-import './App.css'
-import {Button} from "@/components/ui/button.tsx";
+import "./App.css";
+import { Button } from "@/components/ui/button.tsx";
 import React from "react";
-import {clsx} from "clsx";
+import { clsx } from "clsx";
 
 function App() {
-  function getColorPreference(): 'light' | 'dark' {
-    if (matchMedia('(prefers-color-scheme: light)').matches) {
-      return 'light';
-    }
-    
-    return 'dark';
-  }
-  
-  const [isLightMode, toggleLightMode] = React.useState(getColorPreference() === 'light')
-  
-  return (
-    <>
-      <div className={clsx('fixed bottom-0 right-0 w-[50%] h-[100%] bg-white shadow-lg p-4', { container: true, light: isLightMode, dark: !isLightMode})}>
-        <h1 className="font-bold">Deex</h1>
+	function getColorPreference(): "light" | "dark" {
+		if (matchMedia("(prefers-color-scheme: light)").matches) {
+			return "light";
+		}
 
-        <Button onClick={() => toggleLightMode(!isLightMode)}>
-          Hello
-        </Button>
-      </div>
-    </>
-  )
+		return "dark";
+	}
+
+	const [isLightMode, toggleLightMode] = React.useState(
+		getColorPreference() === "light",
+	);
+
+	return (
+		<>
+			<div
+				className={clsx(
+					"fixed bottom-0 right-0 w-[50%] h-[100%] bg-white shadow-lg p-4",
+					{ container: true, light: isLightMode, dark: !isLightMode },
+				)}
+			>
+				<h1 className="font-bold">Deex</h1>
+
+				<Button onClick={() => toggleLightMode(!isLightMode)}>Hello</Button>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
