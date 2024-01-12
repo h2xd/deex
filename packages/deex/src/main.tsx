b3fd97e79d898bdb92915354b9e1c5eabe24d549
Andrej Schujkow <a.schujkow@gmail.com>
2024-01-12 16:26:18 +0100

    feat: add style injection

--- packages/deex/src/main.tsx
+++ packages/deex/src/main.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import * as styles from '../dist2/style.css?raw'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import * as appStyle from "../dist-styles/deex-styles.css?raw";
 
 export function createDeex() {
-  const host = window.document.createElement('div')
-  const shadowRoot = host.attachShadow({ mode: "open" });
-  const root = document.createElement("span");
-  host.id = 'deex-root'
+	const host = window.document.createElement("div");
+	host.id = "deex-root";
 
-  const style = document.createElement('style')
-  console.log(styles)
-  style.innerHTML = styles.default
+	const shadowRoot = host.attachShadow({ mode: "open" });
 
-  shadowRoot.appendChild(root);
-  shadowRoot.appendChild(style);
-  window.document.body.appendChild(host)
+	const root = document.createElement("div");
+	root.className = "bg-white-100 dark:bg-black-900";
 
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
+	const appStyleElement = document.createElement("style");
+	appStyleElement.innerHTML = appStyle.default;
+
+	shadowRoot.appendChild(appStyleElement);
+	shadowRoot.appendChild(root);
+	window.document.body.appendChild(host);
+
+	ReactDOM.createRoot(root).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
 }
--- packages/deex/vite.config.ts
+++ packages/deex/vite.config.ts
@@ -1,34 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: './dist/style.css',
-          dest: '../dist2'
-        }
-      ]
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/main.tsx'),
-      name: 'deex-core',
-      // the proper extensions will be added
-      fileName: 'index',
-    },
-  },
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		lib: {
+			// Could also be a dictionary or array of multiple entry points
+			entry: resolve(__dirname, "src/main.tsx"),
+			name: "deex-core",
+			// the proper extensions will be added
+			fileName: "index",
+		},
+	},
+});
