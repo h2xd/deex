3660f08dbc40eb6a998699c1f7a51bfeb1ff8c0c
Andrej Schujkow <a.schujkow@gmail.com>
2024-01-20 11:07:53 +0100

    feat: add example for extension setup

--- playground/src/components/HelloWorld.vue
+++ playground/src/components/HelloWorld.vue
@@ -1,13 +1,43 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-import { createDeex } from '../../../packages/deex/dist'
+import { createDeex, createDeexExtension } from '../../../packages/deex/dist'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 
+const extension = createDeexExtension({
+  title: 'Deex Extension',
+  description: 'Custom Deex Extension',
+  panels: [
+    {
+      title: 'Panel 1',
+      description: 'Panel 1 Description',
+      components: [
+        {
+          title: 'Reset Count',
+          onClick: (close) => {
+            console.log('Reset Count')
+            count.value = 0
+            close()
+          }
+        },
+         {
+          title: 'Set Count to 100',
+          onClick: (close) => {
+            console.log('Set Count to 100')
+            count.value = 100
+            close()
+          }
+        }
+      ]
+    },
+  ]
+})
+
 const deex = createDeex()
+deex.installExtension(extension)
 </script>
 
 <template>
